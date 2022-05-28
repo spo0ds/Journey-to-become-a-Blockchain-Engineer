4369c67e4f69b7376e7754203ed059ad02dc5335
spo0ds <98577089+spo0ds@users.noreply.github.com>
2022-05-28 02:33:13 +0000

    code added

---
+++ Day04/SimpleStorage.sol
@@ -0,0 +1,27 @@
+pragma solidity ^ 0.6.0;
+
+contract SimpleStorage {
+    uint256 age;
+
+    struct Humans {
+        uint256 age;
+        string name;
+    }
+
+    Humans[] public boys;
+    mapping(string => uint256) public nameToAge;
+
+    function store(uint256 x) public {
+        age = x;
+    }   
+
+    function retrieve() public view returns(uint256) {
+        return age;
+    }
+
+    function addBoy(string memory x, uint256 y) public {
+        boys.push(Humans(y,x)); 
+        nameToAge[x] = y;
+    }
+    
+} 
