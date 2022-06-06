c57a273474ea78418c4d06f12ca8c42968e87ad0
spo0ds <98577089+spo0ds@users.noreply.github.com>
2022-06-06 04:03:24 +0000

    code added for day33

---
+++ Day33/Code/ethers-simple-storage/encryptKey.js
@@ -0,0 +1,21 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY
+  );
+
+  console.log(encryptedJsonKey);
+  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
