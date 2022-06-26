ef22ed332d7bdcf691c9c5043ff395abd7f5341f
Asmit <asmitadhikari540@gmail.com>
2022-06-26 15:59:36 +0545

    helper config modified

--- Day40/Code/hardhat-raffle/helper-hardhat-config.js
+++ Day40/Code/hardhat-raffle/helper-hardhat-config.js
@@ -6,7 +6,7 @@ const networkConfig = {
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subId: "0",
+        subId: "7061",
         callBackGasLimit: "500000",
         interval: "30",
     },
