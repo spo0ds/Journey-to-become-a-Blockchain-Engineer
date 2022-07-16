87c073dd5f81628eda0cb031d41854b6fc8b2a92
Asmit <asmitadhikari540@gmail.com>
2022-07-16 18:09:54 +0545

    code added

---
+++ Day47/Code/hardhat-nft/deploy/01-deploy-basic-nft.js
@@ -0,0 +1,24 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("------------------------")
+    const args = [] // BasicNFT doesn't take any constructor parameters
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: args,
+        logs: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, args)
+    }
+
+    log("--------------------------")
+}
