6001ff273c355bb0e38dafb4b6b99aa1a0ea22fd
Asmit <asmitadhikari540@gmail.com>
2022-07-17 17:11:19 +0545

    code updated

--- Day47/Code/hardhat-nft/contracts/RandomNft.sol
+++ Day47/Code/hardhat-nft/contracts/RandomNft.sol
@@ -4,8 +4,9 @@ pragma solidity 0.8.8;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract RandomNft is VRFConsumerBaseV2 {
+contract RandomNft is VRFConsumerBaseV2, ERC721 {
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
@@ -16,12 +17,15 @@ contract RandomNft is VRFConsumerBaseV2 {
     // VRF Helpers
     mapping(uint256 => address) public s_requestIdToSender;
 
+    // NFT variables
+    uint256 public s_tokenCounter;
+
     constructor(
         address vrfCoordinatorV2,
         uint64 subscriptionId,
         bytes32 gasLane,
         uint32 callbackGasLimit
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random NFT", "RN") {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
@@ -41,7 +45,9 @@ contract RandomNft is VRFConsumerBaseV2 {
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
         address nftOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+        _safeMint(nftOwner, newTokenId);
     }
 
-    function tokenURI(uint256) public {}
+    function tokenURI(uint256) public view override returns (string memory) {}
 }
