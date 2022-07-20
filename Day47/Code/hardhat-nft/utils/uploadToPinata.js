513218bda9e2adc52df48795b4479aa116489a09
Asmit <asmitadhikari540@gmail.com>
2022-07-20 18:36:11 +0545

    pinata script updated

--- Day47/Code/hardhat-nft/utils/uploadToPinata.js
+++ Day47/Code/hardhat-nft/utils/uploadToPinata.js
@@ -26,4 +26,14 @@ async function storeImages(imagesFilePath) {
     return { responses, files }
 }
 
-module.exports = { storeImages }
+async function storeTokenUriMetadata(metadata) {
+    try {
+        const response = await pinata.pinJSONToIPFS(metadata)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+    return null
+}
+
+module.exports = { storeImages, storeTokenUriMetadata }
