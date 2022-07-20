60829d2330319803101eeec8b8747019cebdd3c8
Asmit <asmitadhikari540@gmail.com>
2022-07-20 18:35:45 +0545

    helper hardhat updated

--- Day47/Code/hardhat-nft/helper-hardhat-config.js
+++ Day47/Code/hardhat-nft/helper-hardhat-config.js
@@ -9,6 +9,7 @@ const networkConfig = {
         subId: "7061",
         callBackGasLimit: "500000",
         interval: "30",
+        mintFee: "10000000000000000",
     },
     31337: {
         name: "hardhat",
@@ -16,6 +17,7 @@ const networkConfig = {
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         callBackGasLimit: "500000",
         interval: "30",
+        mintFee: "10000000000000000", // 0.01
     },
 }
 
