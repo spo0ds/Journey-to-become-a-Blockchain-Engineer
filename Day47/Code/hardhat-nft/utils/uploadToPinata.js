8b8460c4a7148e1cc58b38f6caa1931516cca0c9
Asmit <asmitadhikari540@gmail.com>
2022-07-20 14:09:27 +0545

    pinata script updated

---
+++ Day47/Code/hardhat-nft/utils/uploadToPinata.js
@@ -0,0 +1,29 @@
+const pinataSDK = require("@pinata/sdk")
+const fs = require("fs")
+const path = require("path")
+require("dotenv").config()
+
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+
+async function storeImages(imagesFilePath) {
+    const fullImagesPath = path.resolve(imagesFilePath)
+    const files = fs.readdirSync(fullImagesPath)
+    // console.log(files)
+    let responses = []
+    console.log("Uploading to Pinata!")
+    for (fileIndex in files) {
+        const readableStreamForFile = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
+
+        try {
+            const response = await pinata.pinFileToIPFS(readableStreamForFile)
+            responses.push(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { responses, files }
+}
+
+module.exports = { storeImages }
