81f17ca815ec2a6117e09423a71ee3ccc6084591
Asmit <asmitadhikari540@gmail.com>
2022-07-21 17:41:38 +0545

    callanything contract added

---
+++ Day47/Code/hardhat-nft/contracts/subLesson/CallAnything.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.8;
+
+contract CallAnything {
+    address public s_someAddress;
+    uint256 public s_amount;
+
+    function transfer(address someAddress, uint256 amount) public {
+        s_someAddress = someAddress;
+        s_amount = amount;
+    }
+
+    function getSelectorOne() public pure returns (bytes4 selector) {
+        selector = bytes4(keccak256(bytes("transfer(address,uint256)")));
+    }
+
+    function getDataToCallTransfer(address someAddress, uint256 amount)
+        public
+        pure
+        returns (bytes memory)
+    {
+        return abi.encodeWithSelector(getSelectorOne(), someAddress, amount);
+    }
+
+    function callTranferDirectly(address someAddress, uint256 amount)
+        public
+        returns (bytes4, bool)
+    {
+        (bool success, bytes memory returnData) = address(this).call(
+            // getDataToCallTransfer(someAddress, amount)
+            abi.encodeWithSelector(getSelectorOne(), someAddress, amount)
+        );
+        return (bytes4(returnData), success);
+    }
+
+    function callTranferDirectlySig(address someAddress, uint256 amount)
+        public
+        returns (bytes4, bool)
+    {
+        (bool success, bytes memory returnData) = address(this).call(
+            // getDataToCallTransfer(someAddress, amount)
+            abi.encodeWithSignature("transfer(address,uint256)", someAddress, amount)
+        );
+        return (bytes4(returnData), success);
+    }
+}
+
+contract CallFunctionWithoutContract {
+    address public s_selectorsAndSignaturesAddress;
+
+    constructor(address selectorsAndSignaturesAddress) {
+        s_selectorsAndSignaturesAddress = selectorsAndSignaturesAddress;
+    }
+
+    // with a staticcall, we can have this be a view function!
+    function staticCallFunctionDirectly() public view returns (bytes4, bool) {
+        (bool success, bytes memory returnData) = s_selectorsAndSignaturesAddress.staticcall(
+            abi.encodeWithSignature("getSelectorOne()")
+        );
+        return (bytes4(returnData), success);
+    }
+
+    function callTransferFunctionDirectlyThree(address someAddress, uint256 amount)
+        public
+        returns (bytes4, bool)
+    {
+        (bool success, bytes memory returnData) = s_selectorsAndSignaturesAddress.call(
+            abi.encodeWithSignature("transfer(address,uint256)", someAddress, amount)
+        );
+        return (bytes4(returnData), success);
+    }
+}
