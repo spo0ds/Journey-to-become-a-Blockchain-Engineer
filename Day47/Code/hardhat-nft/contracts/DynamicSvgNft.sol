2bb1584bb0541b39893f67b764151bc8dfcf5277
Asmit <asmitadhikari540@gmail.com>
2022-07-22 18:00:08 +0545

    dynamic nft code completed

--- Day47/Code/hardhat-nft/contracts/DynamicSvgNft.sol
+++ Day47/Code/hardhat-nft/contracts/DynamicSvgNft.sol
@@ -2,18 +2,31 @@
 pragma solidity 0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 
 error DynamicSvgNft__NonExistentToken();
 
 contract DynamicSvgNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private immutable i_lowImageURI;
-    string private immutable i_highImageURI;
+    string private i_lowImageURI;
+    string private i_highImageURI;
     string private constant BASE64_ENCODED_SVG_PREFIX = "data:image/svg+xml;base64,";
 
-    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) public s_tokenIdToHighValue;
+
+    event NFTMinted(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
+        i_lowImageURI = svgToImageURI(lowSvg);
+        i_highImageURI = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
@@ -21,16 +34,25 @@ contract DynamicSvgNft is ERC721 {
         return string(abi.encodePacked(BASE64_ENCODED_SVG_PREFIX, svgBase64Encoded));
     }
 
-    function mintNft() public {
-        _safeMint(msg.sender, s_tokenCounter);
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValue[s_tokenCounter] = highValue;
         s_tokenCounter++;
+        _safeMint(msg.sender, s_tokenCounter);
+        emit NFTMinted(s_tokenCounter, highValue);
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         if (!(_exists(tokenId))) {
             revert DynamicSvgNft__NonExistentToken();
         }
-        string memory imageURI = "hi!";
+
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+
+        string memory imageURI = i_lowImageURI;
+
+        if (price >= s_tokenIdToHighValue[tokenId]) {
+            imageURI = i_highImageURI;
+        }
         return
             string(
                 abi.encodePacked(
