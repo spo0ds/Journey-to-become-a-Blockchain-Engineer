39c5d62bc54da0ebc2946b7bd43fc9ffb9f17e3c
spo0ds <asmitadhikari540@gmail.com>
2022-09-25 17:38:12 +0545

    scripts added

---
+++ Day52/Code/hardhat-nft-marketplace/scripts/mint-and-list.js
@@ -0,0 +1,29 @@
+const { ethers } = require("hardhat")
+
+const PRICE = ethers.utils.parseEther("0.1")
+
+async function mintAndList() {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    const basicNft = await ethers.getContract("BasicNft")
+
+    console.log("Minting....")
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+
+    console.log("Approving Nft...")
+    const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId)
+    await approvalTx.wait(1)
+
+    console.log("Listing NFT...")
+    const tx = await nftMarketplace.listItems(basicNft.address, tokenId, PRICE)
+    await tx.wait(1)
+    console.log("Listed!")
+}
+
+mintAndList()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
