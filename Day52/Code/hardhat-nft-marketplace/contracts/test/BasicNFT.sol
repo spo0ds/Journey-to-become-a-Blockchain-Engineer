2e334c343fa7d5c7761fc0bfe5312785c2167556
spo0ds <asmitadhikari540@gmail.com>
2022-09-25 15:51:01 +0545

    code updated

---
+++ Day52/Code/hardhat-nft-marketplace/contracts/test/BasicNFT.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    string public constant TOKEN_URI =
+        "https://ipfs.io/ipfs/Qmcx9T9WYxU2wLuk5bptJVwqjtxQPL8SxjgUkoEaDqWzti?filename=BasicNFT.png";
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Jered", "JD") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+        return s_tokenCounter;
+    }
+
+    function tokenURI(
+        uint256 /*tokenId*/
+    ) public pure override returns (string memory) {
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
--- Day52/Code/hardhat-nft-marketplace/hardhat.config.js
+++ Day52/Code/hardhat-nft-marketplace/hardhat.config.js
@@ -50,6 +50,7 @@ module.exports = {
             // },
             saveDeployments: true,
             chainId: 42,
+            blockConfirmations: 6,
         },
         rinkeby: {
             url: RINKEBY_RPC_URL,
@@ -59,6 +60,7 @@ module.exports = {
             //   },
             saveDeployments: true,
             chainId: 4,
+            blockConfirmations: 6,
         },
         mainnet: {
             url: MAINNET_RPC_URL,
@@ -68,12 +70,14 @@ module.exports = {
             //   },
             saveDeployments: true,
             chainId: 1,
+            blockConfirmations: 6,
         },
         polygon: {
             url: POLYGON_MAINNET_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
             saveDeployments: true,
             chainId: 137,
+            blockConfirmations: 6,
         },
     },
     etherscan: {
