4e07ff596e5c344cd0459aef9458bafe61969013
spo0ds <asmitadhikari540@gmail.com>
2022-09-25 15:50:45 +0545

    deploy script added

---
+++ Day52/Code/hardhat-nft-marketplace/deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    let args = []
+
+    const nftMarketplace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying....")
+        await verify(nftMarketplace.address, args)
+    }
+
+    log("_________________________________________________________")
+}
+
+module.exports.tags = ["all", "nftmarketplace"]
---
+++ Day52/Code/hardhat-nft-marketplace/deploy/02-deploy-basic-nft.js
@@ -0,0 +1,28 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    let args = []
+
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying....")
+        await verify(basicNft.address, args)
+    }
+
+    log("_________________________________________________________")
+
+}
+
+module.exports.tags = ["all", "nftmarketplace"]
