a12ae6560bd8f7ffe98205f681e9270e7cff96b6
spo0ds <asmitadhikari540@gmail.com>
2022-09-26 17:49:03 +0545

    code updated

--- Day53/Code/nextjs-moralis-nft-marketplace/pages/_app.js
+++ Day53/Code/nextjs-moralis-nft-marketplace/pages/_app.js
@@ -1,13 +1,25 @@
 import '../styles/globals.css'
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
+import Head from 'next/head'
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <Header />
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <div>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+        <Header />
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </div>
   )
 }
 
--- Day53/Code/nextjs-moralis-nft-marketplace/pages/index.js
+++ Day53/Code/nextjs-moralis-nft-marketplace/pages/index.js
@@ -1,15 +1,10 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="NFT Marketplace" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+
 
 
     </div>
