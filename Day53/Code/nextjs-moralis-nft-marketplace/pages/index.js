7a2355499b55671e3ef87fc381ce1b90887ce526
spo0ds <asmitadhikari540@gmail.com>
2022-09-28 13:56:50 +0545

    index.js updated to show the attributes of NFT

--- Day53/Code/nextjs-moralis-nft-marketplace/pages/index.js
+++ Day53/Code/nextjs-moralis-nft-marketplace/pages/index.js
@@ -1,12 +1,24 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useMoralisQuery } from "react-moralis"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-
-
-
-    </div>
+  const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+    // TableNAme to search on
+    // Function for the query
+    "ActiveItem",
+    (query) => query.limit(10).descending("tokenId")
   )
+  console.log(listedNfts)
+  return (<div className={styles.container}>
+
+    {fetchingListedNfts ? (<div>Loading...</div>) : listedNfts.map((nft) => {
+      console.log(nft.attributes)
+      const { price, nftAddress, tokenId, marketplaceAddress, seller } = nft.attributes
+      return (<div>
+        Price: {price}. NftAddress: {nftAddress}. TokenId: {tokenId}. Seller: {seller}
+      </div>)
+    })}
+
+  </div>)
 }
