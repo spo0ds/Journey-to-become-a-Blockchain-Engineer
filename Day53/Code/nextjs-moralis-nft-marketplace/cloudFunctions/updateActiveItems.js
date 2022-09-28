60f6ba6941b5a9bbd7386e8a45c3f8ce4c75967d
spo0ds <asmitadhikari540@gmail.com>
2022-09-28 13:57:10 +0545

    code updated

--- Day53/Code/nextjs-moralis-nft-marketplace/cloudFunctions/updateActiveItems.js
+++ Day53/Code/nextjs-moralis-nft-marketplace/cloudFunctions/updateActiveItems.js
@@ -1,33 +1,45 @@
 Moralis.Cloud.afterSave("ItemListed", async (request) => {
     const confirmed = request.object.get("confirmed")
     const logger = Moralis.Cloud.getLogger()
-    logger.info("Looking for confirmed Tx")
+    logger.info("Looking for confirmed TX...")
     if (confirmed) {
-        logger.info("Found Item!")
+        logger.info("Found item!")
         const ActiveItem = Moralis.Object.extend("ActiveItem")
 
+        // In case of listing update, search for already listed ActiveItem and delete
         const query = new Moralis.Query(ActiveItem)
         query.equalTo("nftAddress", request.object.get("nftAddress"))
         query.equalTo("tokenId", request.object.get("tokenId"))
         query.equalTo("marketplaceAddress", request.object.get("address"))
         query.equalTo("seller", request.object.get("seller"))
+        logger.info(`Marketplace | Query: ${query}`)
         const alreadyListedItem = await query.first()
+        console.log(`alreadyListedItem ${JSON.stringify(alreadyListedItem)}`)
         if (alreadyListedItem) {
-            logger.info(`Deleting already listed ${request.object.get("objectId")}`)
+            logger.info(`Deleting ${alreadyListedItem.id}`)
             await alreadyListedItem.destroy()
-            logger.info(`Deleted item with tokenId ${request.object.get("tokenId")} at address ${request.object.get("address")} since it's already been listed.`)
+            logger.info(
+                `Deleted item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get(
+                    "address"
+                )} since the listing is being updated. `
+            )
         }
 
-        const activeItem = new ActiveItem() // creating a new entry
+        // Add new ActiveItem
+        const activeItem = new ActiveItem()
         activeItem.set("marketplaceAddress", request.object.get("address"))
         activeItem.set("nftAddress", request.object.get("nftAddress"))
-        activeItem.set("price", request.oject.get("price"))
+        activeItem.set("price", request.object.get("price"))
         activeItem.set("tokenId", request.object.get("tokenId"))
-        activeItem.set("seller", request.object.set("seller"))
-
-        logger.info(`Adding address: ${request.object.get("address")}, TokenId: ${request.object.get("tokenId")}`)
-        logger.info("Saving....")
-
+        activeItem.set("seller", request.object.get("seller"))
+        logger.info(
+            `Adding Address: ${request.object.get("address")} TokenId: ${request.object.get(
+                "tokenId"
+            )}`
+        )
+        logger.info("Saving...")
         await activeItem.save()
     }
 })
@@ -42,14 +54,23 @@ Moralis.Cloud.afterSave("ItemCanceled", async (request) => {
         query.equalTo("marketplaceAddress", request.object.get("address"))
         query.equalTo("nftAddress", request.object.get("nftAddress"))
         query.equalTo("tokenId", request.object.get("tokenId"))
-        logger.info(`Marketplace | Query ${query}`)
+        logger.info(`Marketplace | Query: ${query}`)
         const canceledItem = await query.first()
-        logger.info(`Marketplace | CanceledItem: ${canceledItem}`)
+        logger.info(`Marketplace | CanceledItem: ${JSON.stringify(canceledItem)}`)
         if (canceledItem) {
-            logger.info(`Deleting ${request.object.get("tokenId")} at address ${request.object.get("address")} since it was canceled.`)
+            logger.info(`Deleting ${canceledItem.id}`)
             await canceledItem.destroy()
+            logger.info(
+                `Deleted item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get("address")} since it was canceled. `
+            )
         } else {
-            logger.info(`No item found with address ${request.object.get("address")} and tokenId ${request.object.get("tokenId")}`)
+            logger.info(
+                `No item canceled with address: ${request.object.get(
+                    "address"
+                )} and tokenId: ${request.object.get("tokenId")} found.`
+            )
         }
     }
 })
@@ -64,14 +85,25 @@ Moralis.Cloud.afterSave("ItemBought", async (request) => {
         query.equalTo("marketplaceAddress", request.object.get("address"))
         query.equalTo("nftAddress", request.object.get("nftAddress"))
         query.equalTo("tokenId", request.object.get("tokenId"))
-        logger.info(`Marketplace | Query ${query}`)
+        logger.info(`Marketplace | Query: ${query}`)
         const boughtItem = await query.first()
-        logger.info(`Marketplace | CanceledItem: ${boughtItem}`)
+        logger.info(`Marketplace | boughtItem: ${JSON.stringify(boughtItem)}`)
         if (boughtItem) {
-            logger.info(`Deleting ${request.object.get("tokenId")} at address ${request.object.get("address")} since it was canceled.`)
-            await canceledItem.destroy()
+            logger.info(`Deleting boughtItem ${boughtItem.id}`)
+            await boughtItem.destroy()
+            logger.info(
+                `Deleted item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get(
+                    "address"
+                )} from ActiveItem table since it was bought.`
+            )
         } else {
-            logger.info(`No item found with address ${request.object.get("address")} and tokenId ${request.object.get("tokenId")}`)
+            logger.info(
+                `No item bought with address: ${request.object.get(
+                    "address"
+                )} and tokenId: ${request.object.get("tokenId")} found`
+            )
         }
     }
 })
