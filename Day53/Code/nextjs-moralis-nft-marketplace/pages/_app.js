c4b166c191854f1e42609f27b2b1a5b15ffdcd7f
spo0ds <asmitadhikari540@gmail.com>
2022-10-02 23:10:24 +0545

    app.js updated with notification provider

--- Day53/Code/nextjs-moralis-nft-marketplace/pages/_app.js
+++ Day53/Code/nextjs-moralis-nft-marketplace/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
 import Head from 'next/head'
+import { NotificationProvider } from 'web3uikit'
 
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
@@ -16,8 +17,10 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <Header />
-        <Component {...pageProps} />
+        <NotificationProvider>
+          <Header />
+          <Component {...pageProps} />
+        </NotificationProvider>
       </MoralisProvider>
     </div>
   )
