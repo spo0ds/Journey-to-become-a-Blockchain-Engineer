8b7498d20339a01e485118d14067b96ed64f0fd0
spo0ds <asmitadhikari540@gmail.com>
2022-10-02 23:10:58 +0545

    updateListingModal component updated

--- Day53/Code/nextjs-moralis-nft-marketplace/components/UpdateListingModal.js
+++ Day53/Code/nextjs-moralis-nft-marketplace/components/UpdateListingModal.js
@@ -1,14 +1,58 @@
-import { Modal, Input } from "web3uikit"
+import { useState } from "react"
+import { useWeb3Contract } from "react-moralis"
+import { Modal, Input, useNotification } from "web3uikit"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import { ethers } from "ethers"
 
-export default function UpdateListingModal({ nftAddress, tokenId, isVisible }) {
+
+export default function UpdateListingModal({ nftAddress, tokenId, isVisible, marketplaceAddress, onClose }) {
+
+    const dispatch = useNotification()
+
+    const [priceToUpdateListingWith, setPriceToUpdateListingWith] = useState(0)
+
+    const handleUpdateListingSuccess = async (tx) => {
+        await tx.wait(1)
+        dispatch({
+            type: "success",
+            message: "listing updated",
+            title: "Listing updated - Please refresh (and move blocks)",
+            position: "topR",
+        })
+        onClose && onClose()
+        setPriceToUpdateListingWith("0")
+    }
+
+    const { runContractFunction: updateListing } = useWeb3Contract({
+        abi: nftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "updateListing",
+        params: {
+            nftAddress: nftAddress,
+            tokenId: tokenId,
+            newPrice: ethers.utils.parseEther(priceToUpdateListingWith || "0"),
+        }
+    })
+
     return (
         <Modal
             isVisible={isVisible}
+            onCancel={onClose}
+            onCloseButtonPressed={onClose}
+            onOk={() => {
+                updateListing({
+                    onError: (error) => console.log(error),
+                    onSuccess: () => handleUpdateListingSuccess()
+                })
+            }}
         >
             <Input
                 label="Update Listing price in L1 Currency(ETH)"
                 name="New listing price"
                 type="number"
+                onChange={(event) => {
+                    setPriceToUpdateListingWith(event.target.value)
+                }}
             />
 
         </Modal>
