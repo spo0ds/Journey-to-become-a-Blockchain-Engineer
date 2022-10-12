746428132abb775d61135e434d34f60abd8e5217
spo0ds <asmitadhikari540@gmail.com>
2022-10-12 11:50:35 +0545

    script updated

--- Day58/Code/hardhat-upgrades/scripts/upgrade-box.js
+++ Day58/Code/hardhat-upgrades/scripts/upgrade-box.js
@@ -1,8 +1,17 @@
 const { ethers } = require("hardhat")
 
 async function main() {
-    const boxProxyAdmin = await ethers. 
- }
+    const boxProxyAdmin = await ethers.getContract("BoxProxyAdmin")
+    const transparentProxy = await ethers.getContract("Box_Proxy")
+    const boxV2 = await ethers.getContract("BoxV2")
+
+    const upgradeTx = await boxProxyAdmin.upgrade(transparentProxy.address, boxV2.address)
+    await upgradeTx.wait(1)
+
+    const proxyBox = await ethers.getContractAt("BoxV2", transparentProxy.address)
+    const version = await proxyBox.version()
+    console.log(version.toString())
+}
 
 main()
     .then(() => process.exit(0))
