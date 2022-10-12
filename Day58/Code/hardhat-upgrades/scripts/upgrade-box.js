ef7ed698c92b2ceb04fa6b3a37ce14465d7b68af
spo0ds <asmitadhikari540@gmail.com>
2022-10-12 10:33:48 +0545

    scripts added

---
+++ Day58/Code/hardhat-upgrades/scripts/upgrade-box.js
@@ -0,0 +1,12 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+    const boxProxyAdmin = await ethers. 
+ }
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
