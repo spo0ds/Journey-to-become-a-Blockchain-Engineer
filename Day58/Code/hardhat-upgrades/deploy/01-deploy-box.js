f1f03233353c82d2b8436730f93282623860ba3d
spo0ds <asmitadhikari540@gmail.com>
2022-10-12 10:33:25 +0545

    deploy script added

---
+++ Day58/Code/hardhat-upgrades/deploy/01-deploy-box.js
@@ -0,0 +1,28 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("-----------------")
+    const box = await deploy("Box", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations,
+        proxy: {
+            proxyContract: "OpenZeppelinTransparentProxy",
+            viaAdminContract: {
+                name: "BoxProxyAdmin",
+                artifact: "BoxProxyAdmin"
+            }
+        }
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(box.address, [])
+    }
+}
---
+++ Day58/Code/hardhat-upgrades/deploy/02-deploy-box2.js
@@ -0,0 +1,21 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("-----------------")
+    const boxv2 = await deploy("BoxV2", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(boxv2.address, [])
+    }
+}
