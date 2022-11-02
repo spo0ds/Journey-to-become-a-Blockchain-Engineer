7102891cf7ea408e44a9db43f63fd15c3da2b840
spo0ds <asmitadhikari540@gmail.com>
2022-11-02 19:34:28 +0545

    governance standard contracts

---
+++ Day59/Code/hardhat-dao/contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract Timelock is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors) {}
+}
