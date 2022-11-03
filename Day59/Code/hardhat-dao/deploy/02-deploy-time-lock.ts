bebbb41eddc356a1d09dcd5faec9bfb8e7761c6a
spo0ds <asmitadhikari540@gmail.com>
2022-11-03 09:40:18 +0545

    deploy script for time lock added

---
+++ Day59/Code/hardhat-dao/deploy/02-deploy-time-lock.ts
@@ -0,0 +1,19 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { MIN_DELAY } from "../helper-hardhat-config"
+
+const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { getNamedAccounts, deployments } = hre
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts();
+
+    log("Deploying Timelock...")
+
+    const timeLock = await deploy("Timelock", {
+        from: deployer,
+        args: [MIN_DELAY, [], []],
+        log: true,
+    })
+}
+
+export default deployTimeLock
