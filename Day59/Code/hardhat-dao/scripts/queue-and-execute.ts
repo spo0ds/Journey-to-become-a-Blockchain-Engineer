2a28b3ec0ba2f79e3a1f3c7c92981eec3a481c89
spo0ds <asmitadhikari540@gmail.com>
2022-11-11 14:33:17 +0545

    queue and execute completed

---
+++ Day59/Code/hardhat-dao/scripts/queue-and-execute.ts
@@ -0,0 +1,40 @@
+import { NEW_STORE_VALUE, FUNC, PROPOSAL_DESCRIPTION, developmentChains, VOTING_DELAY, proposalsFile, MIN_DELAY } from "../helper-hardhat-config"
+import { ethers, network } from "hardhat"
+import { moveBlocks } from "../utils/move-blocks"
+import { moveTime } from "../utils/move-time"
+
+export async function queueAndExecute() {
+    const args = [NEW_STORE_VALUE]
+    const box = await ethers.getContract("Box")
+
+    const encodedFunctionCall = box.interface.encodeFunctionData(
+        FUNC,
+        args
+    )
+
+    const descriptionHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(PROPOSAL_DESCRIPTION))
+
+    const governor = await ethers.getContract("GovernorContract")
+    console.log("Queueing...")
+    const queueTx = await governor.queue([box.address], [0], [encodedFunctionCall], descriptionHash)
+    await queueTx.wait(1)
+
+    if (developmentChains.includes(network.name)) {
+        await moveTime(MIN_DELAY + 1)
+        await moveBlocks(1)
+    }
+
+    console.log("Executing...")
+    const executeTx = await governor.execute([box.address], [0], [encodedFunctionCall], descriptionHash)
+    await executeTx.wait(1)
+
+    const boxNewValue = await box.retrieve()
+    console.log(`New Box Value: ${boxNewValue.toString()}`)
+}
+
+queueAndExecute()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
