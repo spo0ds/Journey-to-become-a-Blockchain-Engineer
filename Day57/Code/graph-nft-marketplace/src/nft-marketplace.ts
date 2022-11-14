2d19f8144c1569673869d3c7d28ad8979266b886
spo0ds <asmitadhikari540@gmail.com>
2022-11-14 13:28:14 +0545

    code updated

--- Day57/Code/graph-nft-marketplace/src/nft-marketplace.ts
+++ Day57/Code/graph-nft-marketplace/src/nft-marketplace.ts
@@ -27,9 +27,57 @@ export function handleItemBought(event: ItemBoughtEvent): void {
   activeItem!.save()
 }
 
-export function handleItemCanceled(event: ItemCanceledEvent): void { }
+export function handleItemCanceled(event: ItemCanceledEvent): void {
+  let itemCanceled = ItemCanceled.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+  let activeItem = ActiveItem.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
 
-export function handleItemListed(event: ItemListedEvent): void { }
+  if (!itemCanceled) {
+    itemCanceled = new ItemCanceled(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    )
+  }
+
+  itemCanceled.seller = event.params.seller
+  itemCanceled.nftAddress = event.params.nftAddress
+  itemCanceled.tokenId = event.params.tokenId
+
+  activeItem!.buyer = Address.fromString("0x000000000000000000000000000000000000dEaD")
+
+  itemCanceled.save()
+  activeItem!.save()
+}
+
+export function handleItemListed(event: ItemListedEvent): void {
+  let itemListed = ItemListed.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+  let activeItem = ActiveItem.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+
+  if (!itemListed) {
+    itemListed = new ItemListed(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    )
+  }
+
+  if (!activeItem) {
+    activeItem = new ActiveItem(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    )
+  }
+
+  itemListed.seller = event.params.seller
+  activeItem.seller = event.params.seller
+
+  itemListed.nftAddress = event.params.nftAddress
+  activeItem.nftAddress = event.params.nftAddress
+
+  itemListed.tokenId = event.params.tokenId
+  activeItem.tokenId = event.params.tokenId
+
+  itemListed.price = event.params.price
+  activeItem.price = event.params.price
+
+  itemListed.save()
+  activeItem.save()
+}
 
 
 function getIdFromEventParams(tokenId: BigInt, nftAddress: Address): string {
